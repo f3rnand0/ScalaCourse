fc00d56b95383c2006f92937986d158fb011b4af
f3rnand1t0 <fgucutito@gmail.com>
2014-09-25 08:31:21 -0500

    added new test cases

--- recfun/src/test/scala/recfun/BalanceSuite.scala
+++ recfun/src/test/scala/recfun/BalanceSuite.scala
@@ -13,15 +13,36 @@ class BalanceSuite extends FunSuite {
     assert(balance("(if (zero? x) max (/ 1 x))".toList))
   }
 
+  test("balance: '(if (zero? x) max / 1 x))' is unbalanced") {
+    assert(!balance("(if (zero? x) max / 1 x))".toList))
+  }
+
   test("balance: 'I told him (that it's not (yet) done).\n(But he wasn't listening)' is balanced") {
     assert(balance("I told him (that it's not (yet) done).\n(But he wasn't listening)".toList))
+  }
+
+  test("balance: 'I told him (that it's not (yet) done).\n(But he wasn't listening' is unbalanced") {
+    assert(!balance("I told him (that it's not (yet) done).\nBut he wasn't listening)".toList))
   }
 
   test("balance: ':-)' is unbalanced") {
     assert(!balance(":-)".toList))
   }
 
-  test("balance: counting is not enough") {
+  test("balance: '())(' counting is not enough") {
     assert(!balance("())(".toList))
   }
+
+  test("balance: '()()' is balanced") {
+    assert(balance("()()".toList))
+  }
+
+  test("balance: '[c * (a + b)] / (c - b)' is balanced") {
+    assert(balance("[c * (a + b)] / (c - b)".toList))
+  }
+
+  test("balance: '(c * (a + b)) / (c - b))' is balanced") {
+    assert(balance("(c * ((a + b) / (c)) - b)".toList))
+  }
+
 }
