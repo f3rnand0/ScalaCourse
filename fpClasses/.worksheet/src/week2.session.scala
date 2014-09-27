a396f10a0999cca0df6b555e9e895d5ec70b5209
Fernando Guerra <fgucutito@gmail.com>
2014-09-27 15:17:22 -0500

    currying week2

--- fpClasses/.worksheet/src/week2.session.scala
+++ fpClasses/.worksheet/src/week2.session.scala
@@ -1,6 +1,6 @@
 package week2
 
-object session {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(222); 
+object session {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(223); 
 
 // Sum of squares
   def sum(f: Int => Int, a: Int, b: Int): Int = {
@@ -9,7 +9,23 @@ object session {;import org.scalaide.worksheet.runtime.library.WorksheetSupport.
       else loop(a + 1, f(a) + acc)
     }
     loop(a, 0)
-  };System.out.println("""sum: (f: Int => Int, a: Int, b: Int)Int""");$skip(25); val res$0 = 
-
-	sum (x => x * x, 3, 5);System.out.println("""res0: Int = """ + $show(res$0))}
+  };System.out.println("""sum: (f: Int => Int, a: Int, b: Int)Int""");$skip(24); val res$0 = 
+	sum (x => x * x, 3, 5);System.out.println("""res0: Int = """ + $show(res$0));$skip(109); 
+	
+	def product(f: Int => Int)(a: Int, b: Int): Int = {
+		if (a > b) 1
+		else f(a) * product(f)(a + 1, b)
+	};System.out.println("""product: (f: Int => Int)(a: Int, b: Int)Int""");$skip(27); val res$1 = 
+	product(x => x * x)(3, 4);System.out.println("""res1: Int = """ + $show(res$1));$skip(44); 
+	
+	def fact(n: Int) = product(x => x)(1, n);System.out.println("""fact: (n: Int)Int""");$skip(9); val res$2 = 
+	fact(5);System.out.println("""res2: Int = """ + $show(res$2));$skip(170); 
+	
+	def mapReduce(f: Int => Int, combine: (Int, Int) => Int, zero: Int)(a: Int, b: Int): Int =
+		if (a > b) zero
+		else combine(f(a), mapReduce(f, combine, zero)(a+1, b));System.out.println("""mapReduce: (f: Int => Int, combine: (Int, Int) => Int, zero: Int)(a: Int, b: Int)Int""");$skip(98); 
+		
+	def productMapR(f: Int => Int)(a: Int, b: Int): Int = mapReduce(f, (x, y) => x * y, 1)(a, b);System.out.println("""productMapR: (f: Int => Int)(a: Int, b: Int)Int""");$skip(31); val res$3 = 
+	productMapR(x => x * x)(3, 4);System.out.println("""res3: Int = """ + $show(res$3))}
+	
 }
--- fpClasses/src/week2/session.sc
+++ fpClasses/src/week2/session.sc
@@ -10,6 +10,25 @@ object session {
     }
     loop(a, 0)
   }                                               //> sum: (f: Int => Int, a: Int, b: Int)Int
-
 	sum (x => x * x, 3, 5)                    //> res0: Int = 50
+	
+	def product(f: Int => Int)(a: Int, b: Int): Int = {
+		if (a > b) 1
+		else f(a) * product(f)(a + 1, b)
+	}                                         //> product: (f: Int => Int)(a: Int, b: Int)Int
+	product(x => x * x)(3, 4)                 //> res1: Int = 144
+	
+	def fact(n: Int) = product(x => x)(1, n)  //> fact: (n: Int)Int
+	fact(5)                                   //> res2: Int = 120
+	
+	def mapReduce(f: Int => Int, combine: (Int, Int) => Int, zero: Int)(a: Int, b: Int): Int =
+		if (a > b) zero
+		else combine(f(a), mapReduce(f, combine, zero)(a+1, b))
+                                                  //> mapReduce: (f: Int => Int, combine: (Int, Int) => Int, zero: Int)(a: Int, b:
+                                                  //|  Int)Int
+		
+	def productMapR(f: Int => Int)(a: Int, b: Int): Int = mapReduce(f, (x, y) => x * y, 1)(a, b)
+                                                  //> productMapR: (f: Int => Int)(a: Int, b: Int)Int
+	productMapR(x => x * x)(3, 4)             //> res3: Int = 144
+	
 }
