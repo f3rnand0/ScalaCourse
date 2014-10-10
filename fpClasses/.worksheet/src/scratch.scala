95bd0abe2e687fac0d0dbd5f18c58e65053bbea3
Fernando Guerra <fgucutito@gmail.com>
2014-10-10 11:13:25 -0500

    end of week3 exercises

---
+++ fpClasses/.worksheet/src/scratch.scala
@@ -0,0 +1,14 @@
+import week3._
+
+object scratch {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(53); val res$0 = 
+  new Rational(1, 2);System.out.println("""res0: week3.Rational = """ + $show(res$0));$skip(51); 
+  
+  def error(msg: String) = throw new Error(msg);System.out.println("""error: (msg: String)Nothing""");$skip(39); 
+  
+  //error("test")
+  
+  val x = null;System.out.println("""x  : Null = """ + $show(x ));$skip(20); 
+  val y: String = x;System.out.println("""y  : String = """ + $show(y ))}
+  
+ //val z: Int = null
+}
